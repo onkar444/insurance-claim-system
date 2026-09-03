d246b5edcdfbfa16c0b7b8c377af1d441bccb55c
onkar444 <onkarraut97@gmail.com>
2026-09-03 23:11:04 +0530

    Refactoring frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import { Status } from './components/Status'
 
 function App() {
+
+  const [status, setStatus] = useState([]);
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/actuator/health");
+        const data = await response.json();
+        setStatus(data.status);
+
+      } catch (error) {
+        console.error(error)
+      };
+
+    }
+    fetchStatus();
+  }, []);
 
   return (
     <>
       <h1>Vite React Application</h1>
-      <h2>Status</h2>
-      <Status />
+      <h2>Status:{status}</h2>
     </>
   )
 }
